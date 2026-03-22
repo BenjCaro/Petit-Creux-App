8544778f58c5ae9002cd86315c8d3be91b7e3bab
BenjCaro <carbenjamin@gmail.com>
2026-03-22 16:18:53 +0100

    add with(category) in Model/Recipe and add section to show results in SearchBar

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -22,7 +22,7 @@ class Recipe extends Model
         'duration',
     ];
 
-    // protected $with= ['category', 'user'];
+    protected $with= ['category'];
 
     protected function casts(): array 
     {
