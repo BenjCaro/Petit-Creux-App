445b5c09ff29d4496ce98222829e7111d0667307
BenjCaro <carbenjamin@gmail.com>
2026-03-22 21:59:18 +0100

    add pagination in ResultsList

--- resources/js/components/Global/ResultsList.vue
+++ resources/js/components/Global/ResultsList.vue
@@ -35,4 +35,23 @@ defineProps<{
             </div>
         </div>
     </section> 
+    <div>
+        <nav v-if="recipes.links.length > 3" class="flex items-center justify-center space-x-1 mt-8">
+            <template v-for="(link, label) in recipes.links" :key="label">
+                <div v-if="link.url === null" 
+                        class="px-4 py-2 text-gray-400 border border-gray-200 rounded-lg text-sm cursor-not-allowed"
+                        v-html="link.label">
+                </div>
+                    <Link v-else 
+                        :href="link.url" 
+                        class="px-4 py-2 border rounded-lg text-sm transition-all duration-200"
+                        :class="{ 
+                            'bg-orange-500 text-white border-orange-500 font-bold': link.active, 
+                            'bg-white text-gray-700 border-gray-200 hover:bg-orange-50 hover:border-orange-200': !link.active 
+                        }"
+                        v-html="link.label"
+                        preserve-scroll/>   
+            </template>
+        </nav>
+    </div>
 </template>
