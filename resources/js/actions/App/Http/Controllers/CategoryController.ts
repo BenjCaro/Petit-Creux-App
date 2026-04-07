7f81900e34779def561644667ac1b207552050ee
BenjCaro <carbenjamin@gmail.com>
2026-04-07 11:28:43 +0200

    fix: remove duplicate import statements in CategoryController, RecipeController, and TwoFactorAuthenticationController

--- resources/js/actions/App/Http/Controllers/CategoryController.ts
+++ resources/js/actions/App/Http/Controllers/CategoryController.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\CategoryController::index
  * @see app/Http/Controllers/CategoryController.php:12
