2aa6844525ebe85a597f59295ce1eb174a2b17b2
BenjCaro <carbenjamin@gmail.com>
2026-04-07 11:36:54 +0200

    fix: remove duplicate import statements in CategoryController, RecipeController, and TwoFactorAuthenticationController

--- resources/js/actions/App/Http/Controllers/CategoryController.ts
+++ resources/js/actions/App/Http/Controllers/CategoryController.ts
@@ -1,5 +1,4 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\CategoryController::index
  * @see app/Http/Controllers/CategoryController.php:12
--- resources/js/actions/App/Http/Controllers/RecipeController.ts
+++ resources/js/actions/App/Http/Controllers/RecipeController.ts
@@ -1,1 +1,106 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
+/**
+* @see \App\Http\Controllers\RecipeController::show
+ * @see app/Http/Controllers/RecipeController.php:11
+ * @route '/recettes/{recipe}'
+ */
+export const show = (args: { recipe: string | { slug: string } } | [recipe: string | { slug: string } ] | string | { slug: string }, options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: show.url(args, options),
+    method: 'get',
+})
+
+show.definition = {
+    methods: ["get","head"],
+    url: '/recettes/{recipe}',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\RecipeController::show
+ * @see app/Http/Controllers/RecipeController.php:11
+ * @route '/recettes/{recipe}'
+ */
+show.url = (args: { recipe: string | { slug: string } } | [recipe: string | { slug: string } ] | string | { slug: string }, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { recipe: args }
+    }
+
+            if (typeof args === 'object' && !Array.isArray(args) && 'slug' in args) {
+            args = { recipe: args.slug }
+        }
+    
+    if (Array.isArray(args)) {
+        args = {
+                    recipe: args[0],
+                }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const parsedArgs = {
+                        recipe: typeof args.recipe === 'object'
+                ? args.recipe.slug
+                : args.recipe,
+                }
+
+    return show.definition.url
+            .replace('{recipe}', parsedArgs.recipe.toString())
+            .replace(/\/+$/, '') + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\RecipeController::show
+ * @see app/Http/Controllers/RecipeController.php:11
+ * @route '/recettes/{recipe}'
+ */
+show.get = (args: { recipe: string | { slug: string } } | [recipe: string | { slug: string } ] | string | { slug: string }, options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: show.url(args, options),
+    method: 'get',
+})
+/**
+* @see \App\Http\Controllers\RecipeController::show
+ * @see app/Http/Controllers/RecipeController.php:11
+ * @route '/recettes/{recipe}'
+ */
+show.head = (args: { recipe: string | { slug: string } } | [recipe: string | { slug: string } ] | string | { slug: string }, options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: show.url(args, options),
+    method: 'head',
+})
+
+    /**
+* @see \App\Http\Controllers\RecipeController::show
+ * @see app/Http/Controllers/RecipeController.php:11
+ * @route '/recettes/{recipe}'
+ */
+    const showForm = (args: { recipe: string | { slug: string } } | [recipe: string | { slug: string } ] | string | { slug: string }, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+        action: show.url(args, options),
+        method: 'get',
+    })
+
+            /**
+* @see \App\Http\Controllers\RecipeController::show
+ * @see app/Http/Controllers/RecipeController.php:11
+ * @route '/recettes/{recipe}'
+ */
+        showForm.get = (args: { recipe: string | { slug: string } } | [recipe: string | { slug: string } ] | string | { slug: string }, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: show.url(args, options),
+            method: 'get',
+        })
+            /**
+* @see \App\Http\Controllers\RecipeController::show
+ * @see app/Http/Controllers/RecipeController.php:11
+ * @route '/recettes/{recipe}'
+ */
+        showForm.head = (args: { recipe: string | { slug: string } } | [recipe: string | { slug: string } ] | string | { slug: string }, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: show.url(args, {
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'HEAD',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'get',
+        })
+    
+    show.form = showForm
+const RecipeController = { show }
+
+export default RecipeController
